ee4ccf642d0f82a7421417af023654acf9323a16
Divyaraj Vaghela <vagheladivyaraj22@gmail.com>
2024-02-03 15:13:55 +0530

    improved readability

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,9 +7,7 @@ function Header()
                 <img src=".\images\Troll_Face.png" className="header--logo--image" alt="Meme Generator logo" />
                 <div className="header--logo--name">Meme Generator</div>
             </div>
-            <div className="header--course">
-                React Course - Project 3
-            </div>
+
         </div>
     )
 }
