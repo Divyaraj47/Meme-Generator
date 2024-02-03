89e111a65cda50f449ba89b5cf83955ecbd820f0
Divyaraj Vaghela <vagheladivyaraj22@gmail.com>
2024-02-03 14:22:22 +0530

    fixed bug of logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header()
     return (
         <div className="header">
             <div className="header--logo">
-                <img src="../images/Troll_Face.png" className="header--logo--image" alt="Meme Generator logo" />
+                <img src=".\images\Troll_Face.png" className="header--logo--image" alt="Meme Generator logo" />
                 <div className="header--logo--name">Meme Generator</div>
             </div>
             <div className="header--course">
